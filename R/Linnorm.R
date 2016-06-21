37211d7b647277fdb0b306f209543a15e881f064
Ken Shun Hang Yip <shunyip@bu.edu>
2016-06-21 23:11:43 +0000

    Add PCA function and update everything
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Linnorm@118834 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Linnorm.R
+++ R/Linnorm.R
@@ -4,7 +4,7 @@
 #' @param datamatrix	The matrix or data frame that contains your dataset. Each row is a feature (or Gene) and each column is a sample (or replicate). Undefined values such as NA are not supported.
 #' @param showinfo Logical. Show lambda value calculated. Defaults to FALSE.
 #' @param method	"default" or "lambda" The program will output the transformed matrix if the method is "default". If the method is "lambda", the program will output a lambda value.
-#' @param minZeroPortion Double >=0, <= 1. Featuress without at least this portion of non-zero values will not be used in the calculation of normalizing parameter. Defaults to 2/3.
+#' @param minZeroPortion Double >=0, <= 1. Only features (genes) with more than this portion of non-zero values will be used in the calculation of normalizing parameter. Defaults to 2/3.
 #' @param perturbation Integer >=2. To search for an optimal minimal deviation parameter (please see the article), Linnorm uses the iterated local search algorithm which perturbs away from the initial local minimum. The range of the area searched in each perturbation is exponentially increased as the area get further away from the initial local minimum, which is determined by their index. This range is calculated by 10 * (perturbation ^ index).
 #' @details  If method is default, Linnorm outputs a transformed expression matrix. For users who wish to work with lambda instead, the output is a single lambda value. Please note that users with the lambda value can obtain a transformed Linnorm dataset by: log1p(lambda * datamatrix). There is no need to rerun the program if a lambda is already calculated.
 #' @return This function returns either a transformed data matrix or a lambda value.
@@ -83,7 +83,7 @@ Linnorm <- function(datamatrix, showinfo = FALSE, method="default",perturbation=
 #' @param output Character. "DEResults" or "Both". Set to "DEResults" to output a matrix that contains Differential Expression Analysis Results. Set to "Both" to output a list that contains both Differential Expression Analysis Results and the transformed data matrix.
 #' @param noINF	Logical. Prevent generating INF in the fold change column by using Linnorm's lambda and adding one. If it is set to FALSE, INF will be generated if one of the conditions has zero expression. Defaults to TRUE. 
 #' @param showinfo Logical. Show lambda value calculated. Defaults to FALSE.
-#' @param minZeroPortion Double >=0, <= 1. Featuress without at least this portion of non-zero values will not be used in the calculation of normalizing parameter. Defaults to 2/3.
+#' @param minZeroPortion Double >=0, <= 1. Only features (genes) with more than this portion of non-zero values will be used in the calculation of normalizing parameter. Defaults to 2/3.
 #' @param perturbation Integer >=2. To search for an optimal minimal deviation parameter (please see the article), Linnorm uses the iterated local search algorithm which perturbs away from the initial local minimum. The range of the area searched in each perturbation is exponentially increased as the area get further away from the initial local minimum, which is determined by their index. This range is calculated by 10 * (perturbation ^ index).
 #' @param robust Logical. In the eBayes function of Limma, run with robust setting with TRUE or FALSE. Defaults to TRUE.
 #' @details  This function performs both Linnorm and limma for users who are interested in differential expression analysis. Please note that if you directly use a Linnorm Nomralized dataset with limma, the output fold change and average expression with be wrong. (p values and adj.pvalues will be fine.) This is because the voom-limma pipeline assumes input to be in raw counts. This function is written to fix this problem.
@@ -219,8 +219,176 @@ Linnorm.limma <- function(datamatrix, design=NULL, output="DEResults", noINF=TRU
 		results <- setNames(listing, c("DEResults", "TMatrix"))
 		return (results)
 	}
-		
+}
+
+
+#' Linnorm-PCA Clustering pipeline for Single Cell Sub-population Analysis
+#'
+#' This function first performs Linnorm transformation on the dataset. Then, it will perform Principal component analysis on the dataset and use k-means clustering to identify subpopulations of cells.
+#' @param datamatrix The matrix or data frame that contains your dataset. Each row is a feature (or Gene) and each column is a sample (or replicate). Undefined values such as NA are not supported.
+#' @param showinfo Logical. Show information about the computing process. Defaults to FALSE.
+#' @param minZeroPortion Double >=0, <= 1. Only features (genes) with more than this portion of non-zero values will be used in the calculation of normalizing parameter. Defaults to 0.5.
+#' @param perturbation Integer >=2. To search for an optimal minimal deviation parameter (please see the article), Linnorm uses the iterated local search algorithm which perturbs away from the initial local minimum. The range of the area searched in each perturbation is exponentially increased as the area get further away from the initial local minimum, which is determined by their index. This range is calculated by 10 * (perturbation ^ index).
+#' @param  num_center Numeric vector. Number of clusters to be tested for k-means clustering. fpc, vegan, mclust and apcluster packages are used to determine the number of clusters needed. If only one number is supplied, it will be used and this test will be skipped. Defaults to c(1:20).
+#' @param  Group Character vector with length equals to sample size. Each character in this vector corresponds to each of the columns (samples) in the datamatrix. This is for plotting purposes only. In the plot, the shape of the points that represent each sample will be indicated by their group assignment. Defaults to NA.
+#' @param  pca.scale Logical. In the prcomp(for Principal component analysis) function, set the "scale." parameter. It signals the function to scale unit variances in the variables before the analysis takes place. Defaults to FALSE.
+#' @param  kmeans.iter Numeric. Number of iterations in k-means clustering. Defaults to 2000.
+#' @details  This function performs PCA clustering using Linnorm transformation.
+#' @return It returns a list with the following results:
+##' \itemize{
+##'  \item{k_means:}{Output of kmeans(for K-means clustering) from the stat package. Note: It contains a "cluster" object that indicates each sample's cluster assignment.}
+##'  \item{PCA:}{Output of prcomp(for Principal component analysis) from the stat package.}
+##'  \item{plot:}{Plot of PCA clustering.}
+##'  \item{Linnorm:}{Linnorm transformed and filtered data matrix.}
+##' }
+#' @keywords Linnorm RNA-seq Raw Count Expression RPKM FPKM TPM CPM normalization transformation Parametric PCA Principal Component Analysis k-means K-means kmeans Clustering
+#' @export
+#' @examples
+#' #Obtain example matrix:
+#' data(Islam2011)
+#' #Example 1
+#' PCA.results <- Linnorm.PCA(Islam2011)
+Linnorm.PCA <- function(datamatrix,showinfo = FALSE, perturbation=10, minZeroPortion=0.5, num_center=c(1:20), Group=NA, pca.scale=FALSE, kmeans.iter=2000) {
+	expdata <- as.matrix(datamatrix)
 	
+	#Linnorm transformation
+	if (length(expdata[1,]) < 2) {
+		stop("Number of samples is less than 2.")
+	}
+	if (length(expdata[,1]) < 10) {
+		stop("Number of features is too small.")
+	}
+	if (perturbation < 2) {
+		stop("perturbation is too small.")
+	}
+	if (minZeroPortion >1 || minZeroPortion < 0) {
+		stop("Invalid minZeroPortion.")
+	}
+	if (anyNA(expdata)) {
+		stop("Dataset contains NA.")
+	}
+	if (sum(which(expdata < 0)) != 0) {
+		stop("Dataset contains negative number.")
+	}
+	if (!is.numeric(num_center)) {
+		stop("num_center must be a vector of integers.")
+	}
+	if (!is.na(Group)) {
+		if (length(Group) != length(expdata[1,])) {
+			stop("Group must be a vector with the same length as sample size.")
+		}
+	}
+	if (kmeans.iter < 10) {
+		stop("kmeans.iter is too small.")
+	}
+	
+	
+	#Step 1: Relative Expression
+	#Turn it into relative expression
+	for (i in seq_along(expdata[1,])) {
+		expdata[,i] <- expdata[,i]/sum(expdata[,i])
+	}
+	#Save the original dataset before trimming.
+	datamatrix <- expdata
+	expdata <- expdata[order(rowMeans(expdata)),]
+	#trim outliers
+	expdata <- expdata[rowSums(expdata != 0) >= (length(expdata[1,]) * minZeroPortion),]
+	
+	X <- LocateLambda(expdata,perturbation)
+	if (showinfo) {
+		message("Lambda is ", X,".",appendLF=TRUE)
+		flush.console()
+	}
+	
+	expdata <- log1p(expdata * X)
+	
+	#Principal Component Analysis
+	res.pca <- prcomp(expdata, scale = pca.scale)
+	
+	#Extract Principal Component 1 and Principal Component 2
+	data <- res.pca[[2]][,1:2]
+	
+	num_clust <- c()
+	if (length(num_center) == 1) {
+		num_clust <- num_center
+	} else {
+		#Automatically determine number of centers by fpc, vegan, mclust and apcluster packages.
+		#fpc
+		pamk.best <- pamk(data,num_center)
+
+		num_clust <- c(pamk.best$nc)
+
+		#vegan
+		fit <- cascadeKM(scale(data, center = TRUE,  scale = TRUE), min(num_center), max(num_center), iter = 500)
+		calinski.best <- as.numeric(which.max(fit$results[2,]))
+
+		num_clust <- c(num_clust, calinski.best)
+		
+		#mclust
+		d_clust <- Mclust(as.matrix(data), G=num_center)
+		m.best <- dim(d_clust$z)[2]
+		
+		num_clust <- c(num_clust, m.best)
+		
+		#apcluster
+		d.apclus <- apcluster(negDistMat(r=2), data)
+		num_clust <- c(num_clust, length(d.apclus@clusters))
+		
+		#Mode of num_clust
+		ux <- unique(num_clust)
+		if (length(ux) == 4) {
+			num_clust <- sort(ux)[2]
+		} else {
+			num_clust <- ux[which.max(tabulate(match(num_clust, ux)))]
+		}
+		
+		
+		if (showinfo) {
+			cat("Number of clusters from the fpc package:", pamk.best$nc, "\n")
+			cat("Number of clusters from the vegan package:", calinski.best, "\n")
+			cat("Number of clusters from the mclust package:", m.best, "\n")
+			cat("Number of clusters from the apcluster package:", length(d.apclus@clusters), "\n")
+			cat("Final number of clusters:", num_clust, "\n")
+		}
+	}
+	
+	#K-means clustering	
+	results <- kmeans(data, num_clust, iter.max = kmeans.iter)
+
+	#Plotting
+	PC1 <- c()
+	PC2 <- c()
+	Cluster <- c()
+	x <- c()
+	y <- c()
+	
+	for (i in 1:length(data[,1])) {
+		PC1 <- c(PC1, data[i,1])
+		PC2 <- c(PC2, data[i,2])
+		Cluster <- c(Cluster, paste("Cluster",results[[1]][i] ))
+	}
+	if (is.na(Group)) {
+		plotdata <- data.frame(PC1=PC1,PC2=PC2,Cluster=Cluster)
+		#Find cluster elipse
+		df_ell <- data.frame()
+		for(g in plotdata$Cluster){
+			df_ell <- rbind(df_ell, cbind(as.data.frame(with(plotdata[plotdata$Cluster==g,], ellipse(cor(PC1, PC2),scale=c(sd(PC1),sd(PC2)),centre=c(mean(PC1),mean(PC2))))),Cluster=g))
+		}
+
+		render_plot <- ggplot_build(ggplot(plotdata, aes(x=PC1, y=PC2, color=Cluster)) + geom_point(aes(shape=Cluster), size = 2) + geom_path(data=df_ell, aes(x=x, y=y,colour=Cluster), size=0.5, linetype=2) + scale_x_continuous("PC1") + scale_y_continuous("PC2") + ggtitle("PCA K-means Clustering") + theme(aspect.ratio=1))
+	} else {
+		plotdata <- data.frame(PC1=PC1,PC2=PC2,Cluster=Cluster,Group=Group)
+		#Find cluster elipse
+		df_ell <- data.frame()
+		for(g in plotdata$Cluster){
+			df_ell <- rbind(df_ell, cbind(as.data.frame(with(plotdata[plotdata$Cluster==g,], ellipse(cor(PC1, PC2),scale=c(sd(PC1),sd(PC2)),centre=c(mean(PC1),mean(PC2))))),Cluster=g))
+		}
+
+		render_plot <- ggplot_build(ggplot(plotdata, aes(x=PC1, y=PC2, color=Cluster)) + geom_point(aes(shape=Group), size = 2) + geom_path(data=df_ell, aes(x=x, y=y,colour=Cluster), size=0.5, linetype=2) + scale_x_continuous("PC1") + scale_y_continuous("PC2") + ggtitle("PCA K-means Clustering") + theme(aspect.ratio=1))
+	}
+	listing <- list(results, res.pca, render_plot, expdata)
+	results <- setNames(listing, c("k_means", "PCA", "plot", "Linnorm"))
+	return (results)
 }
 
 
